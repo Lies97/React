78a42d1d6ca2c4798a0e006d94898f11fe5ea3ed
Nguyen <giaanguyen221197@gmail.com>
2020-10-02 23:48:18 +0700

    Update

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import MovieItem from "./MovieItem";
 import item1 from "../assets/img/minions.jpg";
 import item2 from "../assets/img/home.jpg";
 import item3 from "../assets/img/harvie.jpg";
@@ -6,55 +7,7 @@ import item4 from "../assets/img/minions.jpg";
 class MovieList extends Component {
   render() {
     return (
-      <div>
-        <div className="container mt-5">
-          <h1 className="text-center m-3">LIST MOVIE</h1>
-          <div className="row">
-            <div className="col-sm-3">
-              {/*Begin item */}
-              <div className="card">
-                <img className="card-img-top" src={item1} alt="" />
-                <div className="card-body">
-                  <h4 className="card-title">Minions</h4>
-                  <p className="card-text">85000</p>
-                  <button className="btn btn-primary">Detail</button>
-                </div>
-              </div>
-              {/* End item */}
-            </div>
-            <div className="col-sm-3">
-              <div className="card">
-                <img className="card-img-top" src={item2} alt="" />
-                <div className="card-body">
-                  <h4 className="card-title">Home</h4>
-                  <p className="card-text">85000</p>
-                  <button className="btn btn-primary">Detail</button>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-3">
-              <div className="card">
-                <img className="card-img-top" src={item3} alt="" />
-                <div className="card-body">
-                  <h4 className="card-title">Harvie</h4>
-                  <p className="card-text">85000</p>
-                  <button className="btn btn-primary">Detail</button>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-3">
-              <div className="card">
-                <img className="card-img-top" src={item4} alt="" />
-                <div className="card-body">
-                  <h4 className="card-title">Insideout</h4>
-                  <p className="card-text">85000</p>
-                  <button className="btn btn-primary">Detail</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <MovieItem item1={item1} item2={item2} item3={item3} item4={item4} />
     );
   }
 }
